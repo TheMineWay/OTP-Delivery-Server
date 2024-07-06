aa3d88169cbe52b961e8d82f5ebef96cfcf660b6
Joel Campos <joelcamposoliva9000@gmail.com>
2024-07-06 20:43:23 +0200

    Ensure folders are created

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { existsSync, mkdirSync } from 'fs';
+
+const createFolders = () => {
+  if (!existsSync('/app-data/users')) {
+    mkdirSync('/app-data/users');
+  }
+
+  if (!existsSync('/app-data/inbox')) {
+    mkdirSync('/app-data/inbox');
+  }
+};
 
 async function bootstrap() {
+  createFolders();
+
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
